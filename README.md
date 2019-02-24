1d9812d6c7267875902f85cdbf2c11dad5fc8a7a
lhiep <47427896+lhiep@users.noreply.github.com>
2019-02-23 22:08:17 -0500

    Initialization

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# sos-led-blink
-Arduino blinking an LED to SOS
+# SOS BLINKING
+This is a mini project for ECE387 - Embedded System class at Miami University. In this project, an Arduino is used to blink an LED to SOS. The code is modularized and a demo video is attached below.
+
+[![Thumbnail](https://i9.ytimg.com/vi/so10m-rYnJw/mqdefault.jpg?sqp=CISPyOMF&rs=AOn4CLCbHSf6IOSAr9qv-U6t0kS-hBS-Qw&time=1550977154193)](https://youtu.be/so10m-rYnJw)
---
+++ SOS/SOS.ino
@@ -0,0 +1,37 @@
+/*
+ * Blinks an LED at a given pin to SOS
+ * Code is modularized for ease of use
+ * By: Hiep Le
+ */
+
+// variables
+int led_pin = 7;
+
+void setup() {
+  // pin setup
+  pinMode(led_pin, OUTPUT);
+}
+
+void loop() {
+  // SOS blinking
+  blink_n_times(led_pin, 3, 200, 200);
+  blink_n_times(led_pin, 3, 600, 200);
+  blink_n_times(led_pin, 3, 200, 200);
+  delay(1000);
+}
+
+// blinks an led at the given pin n times
+void blink_n_times(int led, int n, int timeOn, int timeOff) {
+  for(int i=0; i<n; i++) {
+    blink_led(led, timeOn, timeOff);
+  }
+}
+
+// blinks an led at the given pin, on for timeOn amount of time and off for timeOff amount of time
+void blink_led(int led, int timeOn, int timeOff) {
+  digitalWrite(led, HIGH);
+  delay(timeOn);
+  digitalWrite(led, LOW);
+  delay(timeOff);
+}
+
